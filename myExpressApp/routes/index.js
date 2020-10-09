03c383763c299a187545a9150164bf001acd5026
Adam <stargue49@gmail.com>
2020-10-10 03:28:18 +0800

    comment adjustment in home page URL

--- myExpressApp/routes/index.js
+++ myExpressApp/routes/index.js
@@ -2,16 +2,16 @@ var express = require('express');
 var router = express.Router();
 var firstTestObject = require('./Objects/FirstTestObject');
 
-/* GET home page. */
+/* GET home page */
 router.get('/', (req, res) => {
-  //Fetch the ReverseString method and feed it with input string from URL
+  //Request an input from user
   res.send(
     'Add a string extension to the URL to see its corresponding reverse form'
   );
 });
-/* URL for taking input string from user */
+/* Get page displaying reversed input string from user */
 router.get('/:str', (req, res) => {
-  //Fetch the ReverseString method and feed it with input string from URL
+  //Fetch the ReverseString method and feed it with input string from URL extension
   res.send(firstTestObject.methods.ReverseString(req.params.str));
 });
 
